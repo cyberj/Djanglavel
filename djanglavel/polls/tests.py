5e47a7f0daa6f6b34a309e442f697527abd0e56d
Johan Charpentier <cyberj@arcagenis.org>
2015-07-22 16:28:07 +0200

    First view

--- djanglavel/polls/tests.py
+++ djanglavel/polls/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+class PollsTests(TestCase):
+
+    def test_index_page(self):
+        """Simple test for index page
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hello, world. You're at the polls index.")
--- djanglavel/urls.py
+++ djanglavel/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^polls/', include('djanglavel.polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 ]
