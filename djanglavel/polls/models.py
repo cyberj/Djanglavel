41f88b6168dae33f0e0e3aac2a124a1ddea0a5c2
Johan Charpentier <cyberj@arcagenis.org>
2015-07-22 16:09:25 +0200

    Add usefull method

--- djanglavel/polls/models.py
+++ djanglavel/polls/models.py
@@ -1,10 +1,18 @@
+import datetime
 from django.db import models
+from django.utils import timezone
+
 
 class Question(models.Model):
-    """
+    """Simple question for a poll
     """
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        """Return true if published less than 1 day ago
+        """
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
