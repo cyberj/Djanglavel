c3c9418870dfadbf5471f0923490ace802982c96
Johan Charpentier <cyberj@arcagenis.org>
2015-07-23 14:45:14 +0200

    Add detail view

--- djanglavel/polls/views.py
+++ djanglavel/polls/views.py
@@ -11,3 +11,6 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
