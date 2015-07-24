cdc1b9244beb5219c969ee890236dd4d2e0219b2
Johan Charpentier <cyberj@arcagenis.org>
2015-07-24 09:33:13 +0200

    PEP8

--- djanglavel/notebook/models.py
+++ djanglavel/notebook/models.py
@@ -3,6 +3,7 @@ from autoslug import AutoSlugField
 
 
 class Contact(models.Model):
+
     """Contact in a Notebook"""
     first_name = models.CharField('Firstname', max_length=200)
     last_name = models.CharField('Lastname', max_length=200)
@@ -17,6 +18,7 @@ class Contact(models.Model):
 
 
 class Email(models.Model):
+
     """Email for a Contact"""
     contact = models.ForeignKey(Contact)
     email = models.EmailField()
